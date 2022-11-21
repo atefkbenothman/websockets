3fc38ecbaf55b9d8f10a4d2353cd4a9e19293898
atefkbenothman <atefkbenothman@gmail.com>
2022-11-20 17:41:27 -0800

    update

--- server.py
+++ server.py
@@ -1,10 +1,24 @@
 import asyncio
+import datetime
+import json
 import websockets
 
 
 async def handler(websocket) -> None:
   async for message in websocket:
     print(f"server received msg: {message}")
+
+    data = json.loads(message)
+    name = data["name"]
+    msg = data["message"]
+
+    chatObj = {
+      "name": name,
+      "message": msg,
+      "datetime": datetime.datetime.now().strftime("%H:%M:%S")
+    }
+
+    await websocket.send(json.dumps(chatObj))
 
 
 async def main():
